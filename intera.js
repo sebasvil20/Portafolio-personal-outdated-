71169e2602cef8f4e27e3c6e3d3eec7e77d091ce
sebasvil20 <sebasvil20@gmail.com>
2019-03-13 21:48:33 -0500

    Loading page
    Se añade pre-loading page a la pagina

--- intera.js
+++ intera.js
@@ -4,3 +4,12 @@ $(function() {
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
 });
+
+$(document).ready(function() {
+
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+
+});
